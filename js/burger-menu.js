a4ee8a474ac4690521f902f3bad7c899ea68ac8a
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-12-17 23:26:22 +0100

    up burger
    fixed styles

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -19,3 +19,27 @@ closeMenu.addEventListener('click', () => {
     body.classList.remove('no-scroll'); // Відновлюємо скрол
 });
 
+// // Resume scrolling on link click (only for 320px)
+// document.querySelectorAll('.menu ul a').forEach(link => {
+//     link.addEventListener('click', () => {
+//         if (window.innerWidth <= 768) { // Умова: тільки для розміру 320px
+//             menuList.classList.remove('active');
+//             closeMenu.style.display = 'none';
+//             burgerMenu.style.display = 'flex';
+//             body.classList.remove('no-scroll'); // Відновлюємо скрол
+//         }
+//     });
+// });
+
+// Handle window resize (Optional: to ensure proper behavior on resize)
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 767) {
+        menuList.classList.remove('active');
+        closeMenu.style.display = 'none';
+        burgerMenu.style.display = 'none';
+        body.classList.remove('no-scroll'); // Відновлюємо скрол, якщо змінився розмір вікна
+    } else {
+        burgerMenu.style.display = 'flex'; // Показуємо бургер-меню
+    }
+
+});
