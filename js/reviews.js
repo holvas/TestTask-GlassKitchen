5ce18074ba270fc937891ff4c37291ce08d71725
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-12-19 19:44:44 +0100

    finish catalog&create reviews.js
    slider
    fixed styles

--- js/reviews.js
+++ js/reviews.js
@@ -1,1 +1,14 @@
+const reviews = document.querySelectorAll('.review');
+        let currentReview = 0;
 
+        document.querySelector('.next').addEventListener('click', () => {
+            reviews[currentReview].classList.remove('active');
+            currentReview = (currentReview + 1) % reviews.length;
+            reviews[currentReview].classList.add('active');
+        });
+
+        document.querySelector('.prev').addEventListener('click', () => {
+            reviews[currentReview].classList.remove('active');
+            currentReview = (currentReview - 1 + reviews.length) % reviews.length;
+            reviews[currentReview].classList.add('active');
+        });
